776aa4756ac28e1664209189527fd28bfa72b250
juanpa <juanpo.alvarado@gmail.com>
2018-10-13 14:25:16 -0600

    Marco base para pruebas
    Marco muy sencillo que permitirá hacer pruebas sobre el dibujo de las alineaciones y su cambio en caso de morir un dragón

--- src/GUI/Pantalla.java
+++ src/GUI/Pantalla.java
@@ -2,77 +2,94 @@ package GUI;
 
 import adt.B_tree;
 
-import java.util.*;
+import javax.swing.*;
+import java.awt.*;
 
-/**
- * 
- */
+
 public class Pantalla {
 
-    /**
-     * Default constructor
-     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        MarcoCliente mimarco=new MarcoCliente();
+
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+
     public Pantalla() {
     }
 
-    /**
-     * 
-     */
     private String Imagen;
 
-    /**
-     * 
-     */
     private B_tree ArbolB_edad;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /**
-     * 
-     */
     public void Avanzar() {
         // TODO implement here
     }
 
-    /**
-     * 
-     */
     public void Dibujar() {
         // TODO implement here
     }
 
-    /**
-     * 
-     */
     public void VerDragon() {
         // TODO implement here
     }
 
-    /**
-     * 
-     */
     public void VerArbolB() {
         // TODO implement here
     }
 
-    /**
-     * 
-     */
     public void EliminarKey() {
         // TODO implement here
     }
 
+}
+
+class MarcoCliente extends JFrame{
+
+    public MarcoCliente(){
+        Toolkit mipanatalla=Toolkit.getDefaultToolkit();
+
+        Image icono=mipanatalla.getImage("Imagenes/icono.png");
+
+        setIconImage(icono);
+
+        Dimension tamano=mipanatalla.getScreenSize();
+
+        int altopantalla= (int) tamano.getHeight();
+
+        int anchopantalla= (int) tamano.getWidth();
+
+        int alto=600;
+
+        int ancho=800;
+
+        setBounds((anchopantalla-ancho)/2,(altopantalla-alto)/2,ancho,alto);
+
+        setVisible(true);
+
+        setTitle("Pruebas");
+
+        Lamina milamina=new Lamina();
+
+        add(milamina);
+    }
+
+}
+
+class Lamina extends JPanel{
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        for (int i=0;i<15;i++){
+            g.drawRect(100,10+i*35,100,25);
+
+            g.drawString("Hola"+i,120,22+i*35);
+        }
+
+
+    }
 }
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -5,10 +5,5 @@ import adt.Binary_tree;
 public class Principal {
     public static void main(String[] args){
         System.out.println("Hola 2");
-        Binary_tree B1=new Binary_tree();
-        B1.insert(1);
-        B1.insert(2);
-        B1.insert(0);
-        B1.display(B1.root);
     }
 }
