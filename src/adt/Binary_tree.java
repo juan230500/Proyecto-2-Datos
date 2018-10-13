2025b1ca7271bab353c1a8d0d3cc65a84cdd7781
juanpa <juanpo.alvarado@gmail.com>
2018-10-13 11:17:11 -0600

    Recopilación de estructuras de datos
    Estas estructuras se usarán como la lógica para re ordenar los dragones cuando alguno muera de su formación

--- src/adt/Binary_tree.java
+++ src/adt/Binary_tree.java
@@ -1,4 +1,147 @@
 package adt;
+//Código recuperado de https://algorithms.tutorialhorizon.com/binary-search-tree-complete-implementation/
 public class Binary_tree {
+    public Node_BST root;
 
+    public Binary_tree(){
+        this.root = null;
+    }
+
+    public boolean find(int id){
+        Node_BST current = root;
+        while(current!=null){
+            if(current.data==id){
+                return true;
+            }else if(current.data>id){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return false;
+    }
+    public boolean delete(int id){
+        Node_BST parent = root;
+        Node_BST current = root;
+        boolean isLeftChild = false;
+        while(current.data!=id){
+            parent = current;
+            if(current.data>id){
+                isLeftChild = true;
+                current = current.left;
+            }else{
+                isLeftChild = false;
+                current = current.right;
+            }
+            if(current ==null){
+                return false;
+            }
+        }
+        //if i am here that means we have found the node
+        //Case 1: if node to be deleted has no children
+        if(current.left==null && current.right==null){
+            if(current==root){
+                root = null;
+            }
+            if(isLeftChild ==true){
+                parent.left = null;
+            }else{
+                parent.right = null;
+            }
+        }
+        //Case 2 : if node to be deleted has only one child
+        else if(current.right==null){
+            if(current==root){
+                root = current.left;
+            }else if(isLeftChild){
+                parent.left = current.left;
+            }else{
+                parent.right = current.left;
+            }
+        }
+        else if(current.left==null){
+            if(current==root){
+                root = current.right;
+            }else if(isLeftChild){
+                parent.left = current.right;
+            }else{
+                parent.right = current.right;
+            }
+        }else if(current.left!=null && current.right!=null){
+
+            //now we have found the minimum element in the right sub tree
+            Node_BST successor	 = getSuccessor(current);
+            if(current==root){
+                root = successor;
+            }else if(isLeftChild){
+                parent.left = successor;
+            }else{
+                parent.right = successor;
+            }
+            successor.left = current.left;
+        }
+        return true;
+    }
+    public Node_BST getSuccessor(Node_BST deleleNodeBST){
+        Node_BST successsor =null;
+        Node_BST successsorParent =null;
+        Node_BST current = deleleNodeBST.right;
+        while(current!=null){
+            successsorParent = successsor;
+            successsor = current;
+            current = current.left;
+        }
+        //check if successor has the right child, it cannot have left child for sure
+        // if it does have the right child, add it to the left of successorParent.
+//		successsorParent
+        if(successsor!= deleleNodeBST.right){
+            successsorParent.left = successsor.right;
+            successsor.right = deleleNodeBST.right;
+        }
+        return successsor;
+    }
+    public void insert(int id){
+        Node_BST newNodeBST = new Node_BST(id);
+        if(root==null){
+            root = newNodeBST;
+            return;
+        }
+        Node_BST current = root;
+        Node_BST parent = null;
+        while(true){
+            parent = current;
+            if(id<current.data){
+                current = current.left;
+                if(current==null){
+                    parent.left = newNodeBST;
+                    return;
+                }
+            }else{
+                current = current.right;
+                if(current==null){
+                    parent.right = newNodeBST;
+                    return;
+                }
+            }
+        }
+    }
+    public void display(Node_BST root){
+        if(root!=null){
+            display(root.left);
+            System.out.print(" " + root.data);
+            display(root.right);
+        }
+    }
+
+}
+
+class Node_BST {
+    int data;
+    Node_BST left;
+    Node_BST right;
+    public Node_BST(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
---
+++ src/adt/SortArray.java
@@ -0,0 +1,93 @@
+package adt;
+
+public class SortArray {
+    private int[] arr;
+
+    public SortArray(int[] arr) {
+        this.arr = arr;
+    }
+
+    //https://www.geeksforgeeks.org/insertion-sort/
+    void InsertionSort()
+    {
+        int n = arr.length;
+        for (int i=1; i<n; ++i)
+        {
+            int key = arr[i];
+            int j = i-1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j>=0 && arr[j] > key)
+            {
+                arr[j+1] = arr[j];
+                j = j-1;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+    //https://www.geeksforgeeks.org/selection-sort/
+    void SelectionSort(int arr[])
+    {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    //http://www.java2novice.com/java-sorting-algorithms/quick-sort/
+    public void quickSort(int lowerIndex, int higherIndex) {
+
+        int i = lowerIndex;
+        int j = higherIndex;
+        // calculate pivot number, I am taking pivot as middle index number
+        int pivot = arr[lowerIndex+(higherIndex-lowerIndex)/2];
+        // Divide into two arrays
+        while (i <= j) {
+            /**
+             * In each iteration, we will identify a number from left side which
+             * is greater then the pivot value, and also we will identify a number
+             * from right side which is less then the pivot value. Once the search
+             * is done, then we exchange both numbers.
+             */
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while (arr[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                Swap(i, j);
+                //move index to next position on both sides
+                i++;
+                j--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < j)
+            quickSort(lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(i, higherIndex);
+    }
+
+    private void Swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
