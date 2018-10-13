f5c77f8a7a98f576087195aac26bbe51d485aa41
juanpa <juanpo.alvarado@gmail.com>
2018-10-13 14:51:54 -0600

    Evento para imprimir nueva alineacion
    un botón que imprime columnas con texto para representar el evento de colisión de un enemigo y el cambio de alineación

--- src/GUI/Pantalla.java
+++ src/GUI/Pantalla.java
@@ -4,6 +4,8 @@ import adt.B_tree;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Pantalla {
@@ -72,24 +74,57 @@ class MarcoCliente extends JFrame{
 
         setTitle("Pruebas");
 
-        Lamina milamina=new Lamina();
+        Pane pane = new Pane();
 
-        add(milamina);
+        add(pane);
+
+
     }
 
 }
 
-class Lamina extends JPanel{
-    public void paintComponent(Graphics g){
+class Pane extends JPanel implements ActionListener {
 
-        super.paintComponent(g);
+    private JButton next;
+    private int col;
 
-        for (int i=0;i<15;i++){
-            g.drawRect(100,10+i*35,100,25);
+    public Pane(){
+        setBounds(0,0,800,600);
+        setVisible(true);
+        setLayout(null);
+        repaint();
 
-            g.drawString("Hola"+i,120,22+i*35);
-        }
+        next = new JButton("Next");
+        next.setBounds(125,125,100,30);
+        next.addActionListener(this);
+        add(next);
 
+        this.col=0;
 
     }
+
+    public void actionPerformed (ActionEvent e){
+        Object source = e.getSource();
+        if (source == next){
+            Graphics g = this.getGraphics();
+            drawing(g,col);
+        }
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void drawing(Graphics g,int col){
+        int aco=100;
+        for (int i=0;i<15;i++){
+            g.drawRect(100+col*100,10+i*35,100,25);
+
+            g.drawString("Hola"+aco,120+col*100,22+i*35);
+
+            aco+=aco*.2;
+        }
+        this.col++;
+    }
+
 }
