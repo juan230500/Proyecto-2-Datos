60adf1ab20184aae78b45674145dc578c3f1c072
Andrey <andreysansab@gmail.com>
2018-10-18 15:23:46 -0600

    Modificación metodo keyTyped()
    Se agregan las mismas condiciones del método keyPressed() y se imprimen las posiciones X,Y en el momento de presionar algún botón que mueva al caballero.

--- src/juego/Caballero.java
+++ src/juego/Caballero.java
@@ -24,24 +24,53 @@ public class Caballero extends JPanel implements KeyListener {
         add(grifo);
     }
     /**
-     *Verifica cuando se tocan las teclas W,A,S,D que se usan como direccionales
+     *Verifica cuando se tocan las teclas W,A,S,D que se usan como direccionales y cuando se tocan dos de estas al mismo tiempo
      * @param e
      */
 
     @Override
     public void keyTyped(KeyEvent e) {
-        if(e.getKeyChar()== 'w'){
-            grifo.setLocation(grifo.getX(),grifo.getY()-5);
+        if (grifo.getX() + 5 < 380 && grifo.getX() - 5 > -5 && grifo.getY() - 5 > -4 && grifo.getY() + 5 < 380) {
+            if (e.getKeyChar() == 'w' && e.getKeyChar() == 'd') {
+                grifo.setLocation(grifo.getX()+5, grifo.getY() - 5);
+            }
+            if (e.getKeyChar() == 'w' && e.getKeyChar() == 'a') {
+                grifo.setLocation(grifo.getX()- 5, grifo.getY() - 5);
+            }
+            if (e.getKeyChar() == 's' && e.getKeyChar() == 'd') {
+                grifo.setLocation(grifo.getX()+5, grifo.getY() + 5);
+            }
+            if (e.getKeyChar() == 's' && e.getKeyChar() == 'a') {
+                grifo.setLocation(grifo.getX()-5, grifo.getY() + 5);
+            }
+            else if (e.getKeyChar() == 'w') {
+                grifo.setLocation(grifo.getX(), grifo.getY() - 5);
+            }
+            else if (e.getKeyChar() == 's') {
+                grifo.setLocation(grifo.getX(), grifo.getY() + 5);
+            }
+            else if (e.getKeyChar() == 'a') {
+                grifo.setLocation(grifo.getX()-5, grifo.getY());
+            }
+            else if (e.getKeyChar() == 'd') {
+                grifo.setLocation(grifo.getX()+5, grifo.getY() + 5);
+            }
+        }else {
+            if(grifo.getX() + 5 >= 380) {
+                grifo.setLocation(374, grifo.getY());
+            }
+            else if(grifo.getX() - 5 <= -4) {
+                grifo.setLocation(2, grifo.getY());
+            }
+            else if(grifo.getY() + 5 >= 380) {
+                grifo.setLocation(grifo.getX(), 374);
+            }
+            else if(grifo.getY() - 5 <= -4) {
+                grifo.setLocation(grifo.getX(), 6);
+            }
         }
-        if(e.getKeyChar()== 's'){
-            grifo.setLocation(grifo.getX(),grifo.getY()+5);
-        }
-        if(e.getKeyChar()== 'a'){
-            grifo.setLocation(grifo.getX()-5,grifo.getY());
-        }
-        if(e.getKeyChar()== 'd'){
-            grifo.setLocation(grifo.getX()+5,grifo.getY());
-        }
+        System.out.print(grifo.getX() + "\t");
+        System.out.println(grifo.getY());
     }
     /**
      * Verifica cuando se tocan las flechas y cuando se tocan dos de estas al mismo tiempo
@@ -58,21 +87,23 @@ public class Caballero extends JPanel implements KeyListener {
                 grifo.setLocation(grifo.getX() - 5, grifo.getY() + 5);
             }
             if (e.getExtendedKeyCode() == KeyEvent.VK_UP && e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
-                grifo.setLocation(grifo.getX() + 5, grifo.getY() + 5);
+                grifo.setLocation(grifo.getX() + 5, grifo.getY() - 5);
             }
             if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN && e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
                 grifo.setLocation(grifo.getX() + 5, grifo.getY() + 5);
-            } else if (e.getExtendedKeyCode() == KeyEvent.VK_UP) {
+            }
+            else if (e.getExtendedKeyCode() == KeyEvent.VK_UP) {
                 grifo.setLocation(grifo.getX(), grifo.getY() - 5);
-            } else if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN) {
+            }
+            else if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN) {
                 grifo.setLocation(grifo.getX(), grifo.getY() + 5);
-            } else if (e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
+            }
+            else if (e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
                 grifo.setLocation(grifo.getX() - 5, grifo.getY());
-            } else if (e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
+            }
+            else if (e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
                 grifo.setLocation(grifo.getX() + 5, grifo.getY());
             }
-            System.out.print(grifo.getX() + "\t");
-            System.out.println(grifo.getY());
         } else {
             if(grifo.getX() + 5 >= 380) {
                 grifo.setLocation(374, grifo.getY());
@@ -87,6 +118,8 @@ public class Caballero extends JPanel implements KeyListener {
                 grifo.setLocation(grifo.getX(), 6);
             }
         }
+        System.out.print(grifo.getX() + "\t");
+        System.out.println(grifo.getY());
     }
 
 
