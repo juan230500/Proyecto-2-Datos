b79008ffb2f8dbc87b07c584c684bec128d88b19
Andrey <andreysansab@gmail.com>
2018-10-20 01:16:05 -0600

    Método colisionEnem(), y modificaciones paralelas
    Se crea este método para detectar colisiones con los enemigos. Se modifica el constructor para posicionar mejor el caballero y se modifican algunos aspectos para mejorar el funcionamiento del nuevo método.

--- src/juego/Caballero.java
+++ src/juego/Caballero.java
@@ -9,10 +9,12 @@ import java.awt.event.KeyListener;
 /**
  * Clase: Caballero
  * @author Andrey Sanchez
- * @version 18.10.2018
+ * @version 20.10.2018
  */
+
 public class Caballero extends JPanel implements KeyListener {
     private JLabel grifo = new JLabel("Prueba");
+    private boolean choque = false;
 
     /**
      * Default constructor
@@ -20,8 +22,10 @@ public class Caballero extends JPanel implements KeyListener {
 
     public Caballero() {
         addKeyListener(this);
+        setLayout(null);
         setFocusable(true);
         add(grifo);
+        grifo.setBounds(20,100, 80,50);
     }
     /**
      *Verifica cuando se tocan las teclas W,A,S,D que se usan como direccionales y cuando se tocan dos de estas al mismo tiempo
@@ -30,96 +34,94 @@ public class Caballero extends JPanel implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-        if (grifo.getX() + 5 < 380 && grifo.getX() - 5 > -5 && grifo.getY() - 5 > -4 && grifo.getY() + 5 < 380) {
-            if (e.getKeyChar() == 'w' && e.getKeyChar() == 'd') {
-                grifo.setLocation(grifo.getX()+5, grifo.getY() - 5);
+        if (this.choque== false) {
+            if (grifo.getX() + 5 < 380 && grifo.getX() - 5 > -5 && grifo.getY() - 5 > -4 && grifo.getY() + 5 < 380) {
+                if (e.getKeyChar() == 'w' && e.getKeyChar() == 'd') {
+                    grifo.setLocation(grifo.getX() + 5, grifo.getY() - 5);
+                }
+                if (e.getKeyChar() == 'w' && e.getKeyChar() == 'a') {
+                    grifo.setLocation(grifo.getX() - 5, grifo.getY() - 5);
+                }
+                if (e.getKeyChar() == 's' && e.getKeyChar() == 'd') {
+                    grifo.setLocation(grifo.getX() + 5, grifo.getY() + 5);
+                }
+                if (e.getKeyChar() == 's' && e.getKeyChar() == 'a') {
+                    grifo.setLocation(grifo.getX() - 5, grifo.getY() + 5);
+                } else if (e.getKeyChar() == 'w') {
+                    grifo.setLocation(grifo.getX(), grifo.getY() - 5);
+                } else if (e.getKeyChar() == 's') {
+                    grifo.setLocation(grifo.getX(), grifo.getY() + 5);
+                } else if (e.getKeyChar() == 'a') {
+                    grifo.setLocation(grifo.getX() - 5, grifo.getY());
+                } else if (e.getKeyChar() == 'd') {
+                    grifo.setLocation(grifo.getX() + 5, grifo.getY());
+                }
+            } else {
+                if (grifo.getX() + 5 >= 380) {
+                    grifo.setLocation(374, grifo.getY());
+                } else if (grifo.getX() - 5 <= -4) {
+                    grifo.setLocation(2, grifo.getY());
+                } else if (grifo.getY() + 5 >= 380) {
+                    grifo.setLocation(grifo.getX(), 374);
+                } else if (grifo.getY() - 5 <= -4) {
+                    grifo.setLocation(grifo.getX(), 6);
+                }
             }
-            if (e.getKeyChar() == 'w' && e.getKeyChar() == 'a') {
-                grifo.setLocation(grifo.getX()- 5, grifo.getY() - 5);
-            }
-            if (e.getKeyChar() == 's' && e.getKeyChar() == 'd') {
-                grifo.setLocation(grifo.getX()+5, grifo.getY() + 5);
-            }
-            if (e.getKeyChar() == 's' && e.getKeyChar() == 'a') {
-                grifo.setLocation(grifo.getX()-5, grifo.getY() + 5);
-            }
-            else if (e.getKeyChar() == 'w') {
-                grifo.setLocation(grifo.getX(), grifo.getY() - 5);
-            }
-            else if (e.getKeyChar() == 's') {
-                grifo.setLocation(grifo.getX(), grifo.getY() + 5);
-            }
-            else if (e.getKeyChar() == 'a') {
-                grifo.setLocation(grifo.getX()-5, grifo.getY());
-            }
-            else if (e.getKeyChar() == 'd') {
-                grifo.setLocation(grifo.getX()+5, grifo.getY());
-            }
-        }else {
-            if(grifo.getX() + 5 >= 380) {
-                grifo.setLocation(374, grifo.getY());
-            }
-            else if(grifo.getX() - 5 <= -4) {
-                grifo.setLocation(2, grifo.getY());
-            }
-            else if(grifo.getY() + 5 >= 380) {
-                grifo.setLocation(grifo.getX(), 374);
-            }
-            else if(grifo.getY() - 5 <= -4) {
-                grifo.setLocation(grifo.getX(), 6);
-            }
+            System.out.print(grifo.getX() + "\t");
+            System.out.println(grifo.getY());
+            colisionEnem(new JLabel());
         }
-        System.out.print(grifo.getX() + "\t");
-        System.out.println(grifo.getY());
+        else{
+            this.choque = false;
+        }
     }
 
     /**
-     * Verifica cuando se tocan las flechas y cuando se tocan dos de estas al mismo tiempo
+     * Verifica cuando se tocan las flechas y cuando se tocan dos de estas al mismo tiempo, verifica que no exista colision y si la hay "resetea" al caballero
      * @param e
      */
     public void keyPressed(KeyEvent e) {
-        if (grifo.getX() + 5 < 380 && grifo.getX() - 5 > -5 && grifo.getY() - 5 > -4 && grifo.getY() + 5 < 380) {
+        if (this.choque== false) {
+            if (grifo.getX() + 5 < 380 && grifo.getX() - 5 > -5 && grifo.getY() - 5 > -4 && grifo.getY() + 5 < 380) {
 
-            if (e.getExtendedKeyCode() == KeyEvent.VK_UP && e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
-                grifo.setLocation(grifo.getX() - 5, grifo.getY() - 5);
+                if (e.getExtendedKeyCode() == KeyEvent.VK_UP && e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
+                    grifo.setLocation(grifo.getX() - 5, grifo.getY() - 5);
+                }
+                if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN && e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
+                    grifo.setLocation(grifo.getX() - 5, grifo.getY() + 5);
+                }
+                if (e.getExtendedKeyCode() == KeyEvent.VK_UP && e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
+                    grifo.setLocation(grifo.getX() + 5, grifo.getY() - 5);
+                }
+                if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN && e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
+                    grifo.setLocation(grifo.getX() + 5, grifo.getY() + 5);
+                } else if (e.getExtendedKeyCode() == KeyEvent.VK_UP) {
+                    grifo.setLocation(grifo.getX(), grifo.getY() - 5);
+                } else if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN) {
+                    grifo.setLocation(grifo.getX(), grifo.getY() + 5);
+                } else if (e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
+                    grifo.setLocation(grifo.getX() - 5, grifo.getY());
+                } else if (e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
+                    grifo.setLocation(grifo.getX() + 5, grifo.getY());
+                }
+            } else {
+                if (grifo.getX() + 5 >= 380) {
+                    grifo.setLocation(374, grifo.getY());
+                } else if (grifo.getX() - 5 <= -4) {
+                    grifo.setLocation(2, grifo.getY());
+                } else if (grifo.getY() + 5 >= 380) {
+                    grifo.setLocation(grifo.getX(), 374);
+                } else if (grifo.getY() - 5 <= -4) {
+                    grifo.setLocation(grifo.getX(), 6);
+                }
             }
-            if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN && e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
-                grifo.setLocation(grifo.getX() - 5, grifo.getY() + 5);
-            }
-            if (e.getExtendedKeyCode() == KeyEvent.VK_UP && e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
-                grifo.setLocation(grifo.getX() + 5, grifo.getY() - 5);
-            }
-            if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN && e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
-                grifo.setLocation(grifo.getX() + 5, grifo.getY() + 5);
-            }
-            else if (e.getExtendedKeyCode() == KeyEvent.VK_UP) {
-                grifo.setLocation(grifo.getX(), grifo.getY() - 5);
-            }
-            else if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN) {
-                grifo.setLocation(grifo.getX(), grifo.getY() + 5);
-            }
-            else if (e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
-                grifo.setLocation(grifo.getX() - 5, grifo.getY());
-            }
-            else if (e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
-                grifo.setLocation(grifo.getX() + 5, grifo.getY());
-            }
-        } else {
-            if(grifo.getX() + 5 >= 380) {
-                grifo.setLocation(374, grifo.getY());
-            }
-            else if(grifo.getX() - 5 <= -4) {
-                grifo.setLocation(2, grifo.getY());
-            }
-            else if(grifo.getY() + 5 >= 380) {
-                grifo.setLocation(grifo.getX(), 374);
-            }
-            else if(grifo.getY() - 5 <= -4) {
-                grifo.setLocation(grifo.getX(), 6);
-            }
+            System.out.print(grifo.getX() + "\t");
+            System.out.println(grifo.getY());
+            colisionEnem(new JLabel());
         }
-        System.out.print(grifo.getX() + "\t");
-        System.out.println(grifo.getY());
+        else{
+            this.choque = false;
+        }
     }
 
 
@@ -148,6 +150,33 @@ public class Caballero extends JPanel implements KeyListener {
      */
     public void DetectarDragonPasado() {
         // TODO implement here
+    }
+
+    /**
+     *Verifica si el caballero colisiona con un enemigo
+     * @param cho
+     * @return boolean
+     */
+
+    public boolean colisionEnem(JLabel cho){
+        if(cho.getHeight()>this.grifo.getHeight()) { //Delimita quien es mas grande, si ek caballero o el enemigo
+            if (this.grifo.getY() >= cho.getY() && this.grifo.getY() <= cho.getY() + cho.getHeight() && (cho.getX()<=this.grifo.getX()+this.grifo.getWidth()) && (this.grifo.getX()<=cho.getX()+cho.getWidth())) { //Setea los limites de colision en base a quien sea mas pequeño
+                grifo.setLocation(20, 20);
+                System.out.println("Colisiono");
+                return this.choque = true;
+            } else {
+                return this.choque = false;
+            }
+        }
+        else {
+            if (cho.getY() >= this.grifo.getY() && cho.getY() <= this.grifo.getY() + this.grifo.getHeight() && (cho.getX()<=this.grifo.getX()+this.grifo.getWidth()) && (this.grifo.getX()<=cho.getX()+cho.getWidth())) {
+                grifo.setLocation(20, 20);
+                System.out.println("Colisiono");
+                return this.choque = true;
+            } else {
+                return this.choque = false;
+            }
+        }
     }
 
 }
