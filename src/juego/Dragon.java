4a6f93dc8c1cf1079eb4ddb48217075acd3e612b
José Solano <joseandressolanomora@gmail.com>
2018-10-20 19:54:38 -0500

    Pequeñas modificaciones en el orden del código

--- src/juego/Dragon.java
+++ src/juego/Dragon.java
@@ -5,6 +5,14 @@ package juego;
  */
 public class Dragon {
 
+    private String nombre;
+    private int recarga;
+    private int edad;
+    private int resistencia;
+    private String clase;
+    private Dragon padre;
+    private Dragon hijos;
+
     /**
      * Default constructor
      */
@@ -19,97 +27,76 @@ public class Dragon {
         this.setResistencia();
     */}
 
-    /**
-     * 
-     */
-    private String nombre;
-
-    /**
-     * 
-     */
-    private int recarga;
-
-    /**
-     * 
-     */
-    private int edad;
-
-    /**
-     * 
-     */
-    private int resistencia;
-
-    /**
-     * 
-     */
-    private String clase;
-
-    /**
-     * 
-     */
-    private Dragon padre;
-
-    /**
-     * 
-     */
-    private Dragon hijos;
-
 
     public String getNombre() {
         return nombre;
     }
 
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
 
     public int getRecarga() {
         return recarga;
     }
 
+
     public void setRecarga(int recarga) {
         this.recarga = recarga;
     }
+
 
     public int getEdad() {
         return edad;
     }
 
+
     public void setEdad(int edad) {
         this.edad = edad;
     }
+
 
     public int getResistencia() {
         return resistencia;
     }
 
+
     public void setResistencia(int resistencia) {
         this.resistencia = resistencia;
     }
+
 
     public String getClase() {
         return clase;
     }
 
+
     public void setClase(String clase) {
         this.clase = clase;
     }
+
 
     public Dragon getPadre() {
         return padre;
     }
 
+
     public void setPadre(Dragon padre) {
         this.padre = padre;
     }
+
 
     public Dragon getHijos() {
         return hijos;
     }
 
+
     public void setHijos(Dragon hijos) {
         this.hijos = hijos;
     }
+
 
     /**
      * 
