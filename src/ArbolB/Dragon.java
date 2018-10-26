211d142b69bd63a9b84fd5a458d74984a1fb41eb
sahid <sahid.rojas64@gmail.com>
2018-10-25 23:55:39 -0600

    Se crea la clase arbolB como un facade para el arbol B
    se agrega la clase arbolB que simplifica el uso de el arbol

---
+++ src/ArbolB/Dragon.java
@@ -0,0 +1,22 @@
+
+
+import javax.swing.*;
+
+
+/**
+ * 
+ */
+public class Dragon {
+    private String nombre;
+    /**
+     * Default constructor
+     */
+    public Dragon(String nombre) {
+        this.nombre=nombre;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }}
+
